00927b6b3d17e925c3426cd93358ea77e450205a
Kateryna B <butsneva.ekaterina@gmail.com>
2021-05-22 23:54:26 -0500

    print dynamic output

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArr = process.argv.slice(2, process.argv.length);
+console.log(profileDataArr);
 
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+}
+
+printProfileData(profileDataArr)
