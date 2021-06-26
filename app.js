b3e3868aa26dc3112fc4aec6dbf77a3517e1f1e4
Kateryna B <butsneva.ekaterina@gmail.com>
2021-06-25 21:53:38 -0500

    add inquirer

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+const inquirer = require('inquirer')
+// const fs = require('fs')
+// const generatePage = require('./src/page-template')
 
-// ------ capture user input ------ //
-const profileDataArr = process.argv.slice(2)  //before  -  process.argv.slice(2, process.argv.length)
-const [name, github] = profileDataArr
-
-// ------ print dynamic output ------ //
-
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// }
-// printProfileData(profileDataArr)
+// const pageHTML = generatePage(name, github)
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err)
-    console.log('Portfolio complete! Check out index.html to see the output!')
-})
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error (err)
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// })
 
-//When an arrow function has one argument, parentheses are optional. However, when there are no arguments—or more than one—parentheses are necessary.
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ]).then(answers => console.log(answers))
